51ccfcabbf031cd2f2fb6f0ddbfd718e454f9673
Rabindranath <cr20009@ues.edu.sv>
2023-11-06 22:21:22 -0600

    cambios en el telefono

--- src/components/TarjetasCell.vue
+++ src/components/TarjetasCell.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="row contenedores">
-    {{ anuncios }}
+    <p class="lt-md">{{ anuncios }}</p>
+
     <!--tarjeta 1-->
     <div class="col-6 col-md-3" v-for="(dato, i) in anuncios" :key="i">
       <q-card class="my-card-small q-mx-xs" flat bordered>
